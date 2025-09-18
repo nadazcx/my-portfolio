48bebe7b4c75aa863ef7b7075a4d4d078fbdda00
Nada <zcnadaa@gmail.com>
2025-09-19 00:05:19 +0100

    git push

---
+++ fetch.js
@@ -0,0 +1,95 @@
+const fs = require("fs");
+const https = require("https");
+require("dotenv").config();
+
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
+
+const ERR = {
+  noUserName:
+    "Github Username was found to be undefined. Please set all relevant environment variables.",
+  requestFailed:
+    "The request to GitHub didn't succeed. Check if GitHub token in your .env file is correct."
+};
+
+if (USE_GITHUB_DATA === "true") {
+  if (!GITHUB_USERNAME) {
+    throw new Error(ERR.noUserName);
+  }
+
+  console.log(`Fetching profile data for ${GITHUB_USERNAME}`);
+
+  const data = JSON.stringify({
+    query: `
+{
+  user(login:"${GITHUB_USERNAME}") { 
+    name
+    bio
+    avatarUrl
+    location
+    pinnedItems(first: 6, types: [REPOSITORY]) {
+      totalCount
+      edges {
+        node {
+          ... on Repository {
+            name
+            description
+            forkCount
+            stargazers {
+              totalCount
+            }
+            url
+            id
+            diskUsage
+            primaryLanguage {
+              name
+              color
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+  });
+
+  const options = {
+    hostname: "api.github.com",
+    path: "/graphql",
+    port: 443,
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+      "User-Agent": "Node"
+    }
+  };
+
+  const req = https.request(options, res => {
+    let body = "";
+
+    console.log(`statusCode: ${res.statusCode}`);
+    if (res.statusCode !== 200) {
+      throw new Error(ERR.requestFailed);
+    }
+
+    res.on("data", chunk => {
+      body += chunk;
+    });
+
+    res.on("end", () => {
+      fs.writeFile("./public/profile.json", body, function (err) {
+        if (err) return console.log(err);
+        console.log("✅ saved file to public/profile.json");
+      });
+    });
+  });
+
+  req.on("error", error => {
+    throw error;
+  });
+
+  req.write(data);
+  req.end();
+}
