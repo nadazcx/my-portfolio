5943fcda80c67504a002d20ecd7d4a0a21ea55fa
Nada <zcnadaa@gmail.com>
2025-09-20 19:11:12 +0100

    removing unused functions

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,6 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
 import {Fade} from "react-reveal";
-import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   return (
