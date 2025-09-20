d62567e636d0eaf3f71aa4f9d86a34f4b6d40620
Nada <zcnadaa@gmail.com>
2025-09-20 18:53:07 +0100

    modification du headline

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Nada Zammit Chatti",
-  title: "Hi all, I'm Nada",
+  title: "CouCou , I'm Nada",
   subTitle: emoji(
     "A hybrid software engineer 🚀 who can build everything from software and websites to full data pipelines."
   ),
